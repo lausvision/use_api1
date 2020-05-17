f51904f505a17e439f2d75c2807ad751beb9e3eb
lausvision <lauramolinalopez02@gmail.com>
2020-05-17 11:16:47 +0200

    tutorial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+        items:[],
+        isLoaded:false,
+
+    }
+  }
+
+  componentDidMount(){
+
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(json => {
+      this.setState({
+        isLoaded: true,
+        items: json,
+      })
+    })
+  }
+
+  render (){
+
+      var {isLoaded,items}= this.state;
+
+      if(!isLoaded){
+        return <div>Loading...</div>;
+      }else{
+        return(
+          <div className="App">
+       
+              <ul>
+                {items.map(item => (
+                    <li key= {item.id}>
+                      {item.name} | Email: :{item.email}
+                    </li>
+                ))};
+              </ul>
+
+          </div>
+        );
+      }
+
+      
+   
+  }
+   
+  
 }
 
 export default App;
